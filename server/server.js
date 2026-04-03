d1e531f12404f893e8eed746d009119ebd73650a
iqraonline2025-lang <iqraonline2025@gmail.com>
2026-04-03 11:39:18 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.enable('trust proxy');
 
 // 1. SESSION CONFIG
 app.use(
